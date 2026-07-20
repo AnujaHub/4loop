ceadda775ad109b31c32a573aeb9fbfe01b44187
Smita Bhoine <144260448+SmitaB007@users.noreply.github.com>
2026-07-20 21:57:09 +0530

    first commit
    Add a method to print a pyramid pattern of stars.

--- smita/first.java
+++ smita/first.java
@@ -1,1 +1,21 @@
 
+class Main{
+    public void patt(int n){
+for(int i=0;i<n;i++){
+    for(int j=0;j<n-1-i;j++){
+        System.out.print(" ");
+    }
+    for(int k=0;k<2*i+1;k++){
+        System.out.print("*");
+    }
+    for(int l=0;l<n-1-i;l++){
+        System.out.print(" ");
+    }
+System.out.println();
+}
+    }
+    public static void main(String args[]){
+     Main m=new Main();
+     m.patt(3);
+    }
+}
